5f1cd2cc3dd29cdf3dfdc8740b37e2f543b4fbfa
halisson1011 <you@example.com>
2025-09-20 10:56:53 -0300

    Remocao CicdApplicatin Nome errado para DemoApplication

--- src/main/java/br/com/empresa/gerenciadortarefas/DemoApplication.java
+++ src/main/java/br/com/empresa/gerenciadortarefas/DemoApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CicdApplication.class, args);
-	}
-
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
